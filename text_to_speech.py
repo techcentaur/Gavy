332b9d62be0e0aa34b5ec3bf02aa2bfe1df78ea0
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-12-07 23:26:52 +0530

    Use Google Translate TTS for speech

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,7 +1,26 @@
-import pyttsx3
+# import pyttsx3
 
 
-def say_gavy(str):
-    engine = pyttsx3.init()
-    engine.say(str)
-    engine.runAndWait()
+# def say_gavy(str):
+#     engine = pyttsx3.init()
+#     engine.say(str)
+#     engine.runAndWait()
+
+
+
+from pygame import mixer
+from gtts import gTTS
+import time
+import os
+
+def say_gavy(text):
+	tts = gTTS(text=text, lang='en')
+	mixer.init()
+	tts.save('temp.mp3')
+
+
+	with open('temp.mp3') as f:
+		mixer.music.load('temp.mp3')
+		mixer.music.play()
+		
+	os.remove('temp.mp3')
