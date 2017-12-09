c986e7deda68cd5eaf77ba8fccca0acdedbe2938
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-12-09 08:01:40 +0530

    Fix some mistakes

--- begin.py
+++ begin.py
@@ -3,66 +3,60 @@ import output, listen
 import helper
 from tasks import Manager
 from NLPapi import main
+import helper
 
 
 
-class Begin:
+class Gavy:
+    def __init__(self, listen, speak):
+        """Initialises the object variables
+        """
+        self.listen = listen
+        self.speak = speak
+        self.introduction()
 
+    def introduction(self):
+        """Greetings and introductions. The formalities before the real fun.
+        """
+        self.user = 'Ankit'
+        self.name = 'Gavy'
 
-    def __init__(self):
-        """intialises the variables"""
-
-        self.listen = listen.SpeechInput()
-        self.speak = output.GTTSOutput()
-        self.names()
-
-
-    def begin(self):
-        """takes the query as an input and starts processing it"""
-
-        self.speak.output("How can I help you Sir")
-        query = self.listen.input()
+        self.speak.output('Are you {}?'.format(self.user))
+        response = self.listen.input()
         
-        query=query.lower()
-        processing_task(query)
+        if not helper.positive(response):
+            self.speak.output('Can you please tell me who you are?')
+            self.user = self.listen.input()
+        
+        self.speak.output('Hi! {user}! I am {name}'.format(user=self.user, name=self.name))
+        self.speak.output('Do you want to give me a new name?') 
+        response = self.listen.input()
+        
+        if helper.positive(response):
+            self.speak.output('What should my name be?')
+            self.name = self.listen.input()
+            self.speak.output('So, for this session I am {name}.'.format(name=self.name))
 
+    def getquery(self):
+        """Takes the query as an input and starts processing it
+        """
+        self.speak.output('How can I help you {}'.format(self.user))
+        query = self.listen.input().lower()
+        
+        self.processing_task(query)
 
-    def processing_task(self,query):
-        """sending the query to the NLP-api main class"""
-
+    def execute_query(self,query):
+        """Sending the query to the NLP-api main class
+        """
         task = main.Main(query, [self.user,self.name],[self.listen,self.speak])
         
         action = t1.action_work()
         main.main(action,task)
 
 
-    def names(self):
-        """initiats the variables and begins the converstaion"""
 
-        self.user="Ankit"
-        self.name="Gavy"
-
-        self.speak.output("Are you " + user + " ?")
-        ch = self.listen.input()
-
-        for n in nolist :
-            if n in ch:
-                self.speak.output("Can you please tell me who you are?")
-                self.user = listen.input()
-
-        self.speak.input("Hi! "+user+" sir. My name is " + name)
-        self.speak.input("Do you want to give me a new name")
-        
-        ch = self.listen.input()
-        if helper.positive(ch)
-            self.speak.input("What should my name be?")
-            
-            self.name = self.listen.input()
-            self.speak.output("so, for this session I am "+name +".")
-
-
-
-
-if __name__=="__main__":
-    b = Begin()
-    b.begin()
+if __name__=='__main__':
+    listen = listen.SpeechInput()
+    with output.GTTSOutput() as speak:
+        gavy = Gavy(listen, speak)
+        gave.execute()
