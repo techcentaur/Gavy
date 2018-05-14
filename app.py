865ad53fee62d3acb0baaf54fd088c9ab03aa5da
Ankit Solanki <ankit03june@gmail.com>
2018-05-15 01:21:29 +0530

    EDIT: app.py modified with new calls and functions

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-import output, listen
-import helper, NLPapi
-import nltk, argparse
 import yaml
+import output, listen
+import nltk, argparse
+from language import api
 
 class Gavy:
     def __init__(self, listen, speak):
@@ -13,34 +13,32 @@ class Gavy:
         with open('_config.yml', 'r') as ymlfile:
             config = yaml.load(ymlfile)
         
-        self.user = config['name']['username']
-        self.name = config.appname['name']['appname']
+        self.username = config['name']['username']
+        self.appname = config['name']['appname']
 
         self.intro()
 
 
     def intro(self):
-        self.speak.output('Hi {user}! I am {name}.'.format(user=self.user, name=self.name))
+        self.speak.output('Hi {user}! I am {name}.'.format(user=self.username, name=self.appname))
 
 
     def query(self):
-        """Takes the query as an input and starts processing it"""
-        self.speak.output('How can I help you {}.'.format(self.user))
-        self.query = self.listen.input().lower()
+        self.speak.output('How can I help you {} ?'.format(self.username))
+        query = self.listen.input().lower()
         
-        self.process()
+        param_dict = dict(query = query, username = self.username, appname = self.appname, input = self.listen, output = self.speak)
+        lang = api.Language(param_dict)
 
 
-    def process(self):
-        """sending the query to the NLP-api main class"""
-        t1 = NLPapi.main.Main(self.query, [self.user,self.name], [self.listen,self.speak])
-        
         action = t1.action_work()
         t2 = manager.TasksManager(self.listen,self.speak)
         t2.execute(action[0],action[1],self.query)
 
-def main():
-    parser = argparse.ArgumentParser(description='A CLI based virtual assistant.')
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='Gavy: A CLI based virtual assistant.')
     group = parser.add_mutually_exclusive_group(required=True)
 
     group.add_argument("-t", "--terminal", help='interaction through terminal', action='store_true')
@@ -53,13 +51,8 @@ def main():
         speak = output.ConsoleOutput()
         gavy = Gavy(listen, speak)
         gavy.query()
-    else if args.speech:
+    elif args.speech:
         listen = listen.SpeechInput()
         with output.GTTSOutput() as speak:
             gavy = Gavy(listen, speak)
             gavy.query()
-
-
-
-if __name__=='__main__':
-    main()
